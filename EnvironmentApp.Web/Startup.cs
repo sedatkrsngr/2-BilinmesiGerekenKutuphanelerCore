e16efb368bebac676c8b56d4ab49e656d87c8d50
sedatkrsngr <80162533+sedatkrsngr@users.noreply.github.com>
2021-03-28 15:30:06 +0300

    Update Startup.cs

--- EnvironmentApp.Web/Startup.cs
+++ EnvironmentApp.Web/Startup.cs
@@ -20,27 +20,24 @@ namespace EnvironmentApp.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else if (env.IsEnvironment("Alfa"))//Kendimizin tanýmladýðý ortama böyle gireriz
+            else if (env.IsEnvironment("Alfa"))//Kendimizin tanÃ½mladÃ½Ã°Ã½ ortama bÃ¶yle gireriz
             {
 
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
